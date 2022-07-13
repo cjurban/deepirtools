bedbeb545071e6cda4f7579ba58f2b597a20e264
cjurban <40474201+cjurban@users.noreply.github.com>
2022-07-13 14:48:15 -0400

    Update test_iwave.py

--- tests/test_iwave.py
+++ tests/test_iwave.py
@@ -3,30 +3,35 @@ import torch
 import deepirtools
 
 
-seed = 1234
+deepirtools.manual_seed(1234)
+torch.set_default_dtype(torch.float64)
+devices = ["cpu"]
+if torch.cuda.is_available():
+    devices.append("cuda")
+
 sample_size = 1000
 batch_size = 32
 
 
-def test_grm():
-    deepirtools.manual_seed(seed)
+@pytest.mark.parametrize("device", devices)
+def test_grm(device):
     
-    
-def test_gpcm():
-    deepirtools.manual_seed(seed)
-    
-    
-def test_poisson_factor_model():
-    deepirtools.manual_seed(seed)
-   
 
-def test_negative_binomial_factor_model():
-    deepirtools.manual_seed(seed)
+@pytest.mark.parametrize("device", devices)
+def test_gpcm(device):
     
     
-def test_normal_factor_model():
-    deepirtools.manual_seed(seed)
+@pytest.mark.parametrize("device", devices)   
+def test_poisson_factor_model(device):
+
     
+@pytest.mark.parametrize("device", devices)    
+def test_negative_binomial_factor_model(device):
+
     
-def test_lognormal_factor_model():
-    deepirtools.manual_seed(seed)
+@pytest.mark.parametrize("device", devices)    
+def test_normal_factor_model(device):
+    
+
+@pytest.mark.parametrize("device", devices)
+def test_lognormal_factor_model(device):
