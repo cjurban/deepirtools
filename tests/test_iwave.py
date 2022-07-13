c80620f6161434a5e066731cb3269cd995615295
cjurban <40474201+cjurban@users.noreply.github.com>
2022-07-13 14:39:56 -0400

    Create test_iwave.py

---
+++ tests/test_iwave.py
@@ -0,0 +1,32 @@
+import pytest
+import torch
+import deepirtools
+
+
+seed = 1234
+sample_size = 1000
+batch_size = 32
+
+
+def test_grm():
+    deepirtools.manual_seed(seed)
+    
+    
+def test_gpcm():
+    deepirtools.manual_seed(seed)
+    
+    
+def test_poisson_factor_model():
+    deepirtools.manual_seed(seed)
+   
+
+def test_negative_binomial_factor_model():
+    deepirtools.manual_seed(seed)
+    
+    
+def test_normal_factor_model():
+    deepirtools.manual_seed(seed)
+    
+    
+def test_lognormal_factor_model():
+    deepirtools.manual_seed(seed)
